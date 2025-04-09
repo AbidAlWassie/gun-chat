86a7ab39c01056ec5bb3ddea94a9b8071cce8d25
Abid Al Wassie <abidalwassie@outlook.com>
2025-04-09 09:11:58 +0600

    added server env and cors

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,26 @@
 // server/index.ts
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import Gun from "gun";
-import path from "path";
+dotenv.config();
 
-const GUN = Gun as unknown as (opts?: { web: any; file?: string }) => any;
+const CLIENT_URL: string = process.env.CLIENT_URL as string;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  })
+);
+
+const GUN = Gun as unknown as (opts?: { web: any; file?: string }) => any;
 const server = app.listen(8765, () => {
   console.log("Relay peer running on port 8765");
 });
 
 GUN({
   web: server,
-  file: path.resolve(__dirname, "server/radata"), // Absolute path
 });
